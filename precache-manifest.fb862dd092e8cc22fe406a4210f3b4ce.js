75230432e6417f658d7f5952df94882915387cdd
shreyaa1 <shuklashreya801@gmail.com>
2019-11-13 13:52:43 +0530

    Updates

---
+++ precache-manifest.fb862dd092e8cc22fe406a4210f3b4ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e723c538038e829f279b9b69ca0b604",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd20627f04ad0c7bd867",
+    "url": "/static/css/main.4f5879cd.chunk.css"
+  },
+  {
+    "revision": "3ff1154c177e36d2cc50",
+    "url": "/static/js/2.07911c8f.chunk.js"
+  },
+  {
+    "revision": "fd20627f04ad0c7bd867",
+    "url": "/static/js/main.9a494cf2.chunk.js"
+  },
+  {
+    "revision": "a8d086a6f31f97580973",
+    "url": "/static/js/runtime-main.03e2fdf0.js"
+  },
+  {
+    "revision": "6f9673d6ac1bf81802ccbba9efe91d2f",
+    "url": "/static/media/hero-image.6f9673d6.png"
+  }
+]);
